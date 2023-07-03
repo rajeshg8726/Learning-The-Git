a0d6ce6304f75388724fbc915178eb8b7903a1a2
Rajesh Gupta <rajeshg8726@gmail.com>
2023-07-03 16:19:18 +0530

    This is the second commit after some modification

--- Demo.cpp
+++ Demo.cpp
@@ -7,7 +7,7 @@ using namespace std;
 int main()
 {
 
-    cout<<"This is not a commited file for github"<<endl;
+    cout<<"This i s a commited file for github"<<endl;
     cout<<"I am trying to learn the git and github.";
 
 
