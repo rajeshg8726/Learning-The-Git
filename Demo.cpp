cb1b942f104e9de9cb4ad6d28abd7580abcb537b
Rajesh Gupta <rajeshg8726@gmail.com>
2023-07-03 16:10:49 +0530

    This is the first commit of my learning

---
+++ Demo.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+
+
+int main()
+{
+
+    cout<<"This is not a commited file for github"<<endl;
+    cout<<"I am trying to learn the git and github.";
+
+
+    return 0;
+}
